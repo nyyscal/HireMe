626a5a9fb6f845550f351dc4408bfbcf60e7856f
Nischal <nyyscal541556@gmail.com>
2026-03-18 10:09:29 +0545

    Inngest Name Conflict

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -5,34 +5,45 @@ import {User} from "../models/user.model.js"
 export const inngest = new Inngest({id:"hire-me"})
 
 const syncUser = inngest.createFunction(
-  {id:"sync-user"},
+  {id:"hireme_sync-user"},
   {event:"clerk/user.created"},
   async({event})=>{
-    await connectDB()
-    const {id,email_addresses, first_name, last_name, image_url} = event.data
-    //Clerk Sends these fields via webhooks
-    const newUser = {
-      clerkId:id,
-      email: email_addresses[0]?.email_address,
-      name:`${first_name || ""}${last_name||""}` ||"User",
-      imageUrl: image_url,
-      addresses: [],
-      wishlist: []
+    try{
+      await connectDB()
+      const {id,email_addresses, first_name, last_name, image_url} = event.data
+      if (!id) throw new Error("Clerk user ID missing");
+      //Clerk Sends these fields via webhooks
+      const newUser = {
+        clerkId:id,
+        email: email_addresses?.[0]?.email_address || "",
+        name:`${first_name || ""} ${last_name||""}` ||"User",
+        imageUrl: image_url || "",
+        addresses: [],
+        wishlist: []
+      }
+      await User.create(newUser)
+    } catch(error){
+      console.log("Error syncing user to the database.")
+      throw error
     }
-    await User.create(newUser)
   }
 )
 
 const deleteUserFromDB = inngest.createFunction(
-  {id:"delete-user-from-db"},
+  {id:"hireme_delete-user-from-db"},
   {event:"clerk/user.deleted"},
   //Delete user using webhooks
   async({event})=>{
+  try {
     await connectDB()
     
     const {id} = event.data
     await User.deleteOne({clerkId: id})
+  }catch (error) {
+     console.log("Error deleting user from the database.")
+     throw error
   }
+}
 )
 
 export const functions = [syncUser, deleteUserFromDB]
--- backend/src/server.js
+++ backend/src/server.js
@@ -10,16 +10,28 @@ const app = express()
 const PORT = ENV.PORT
 
 app.use(express.json())
+
+//From docs of inngest
+app.use("/api/inngest",serve({client: inngest, functions}))
+
 app.use(clerkMiddleware())
 
-app.use("/api/inngest",serve({client: inngest, functions}))
-//From docs of inngest
 
 app.get("/api/health",(_,res)=>{
   res.send("Server is healthy!")
+  // console.log(process.env.INNGEST_SIGNING_KEY)
 })
 
-app.listen(PORT,()=>{
-  connectDB()
-  console.log(`🚀 Server is running in http://localhost:${PORT}`)
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to connect DB:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
